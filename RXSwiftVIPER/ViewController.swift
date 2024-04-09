ed3deb8e9a03da3a8aecc5131ea154ee60258cb4
vishelpodishat <alishsaideshov@gmail.com>
2024-04-09 23:53:24 +0500

    Initial Commit

---
+++ RXSwiftVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RXSwiftVIPER
+//
+//  Created by Alisher Saideshov on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
