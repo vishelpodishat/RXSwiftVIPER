6e16008cd613fea9ab6d651c14cf3fbe285ddeb2
vishelpodishat <alishsaideshov@gmail.com>
2024-04-17 16:03:23 +0500

    init commit mvvm rx

---
+++ RXSwiftVIPER/MVVM/MVVMModel.swift
@@ -0,0 +1,13 @@
+//
+//  MVVMModel.swift
+//  RXSwiftVIPER
+//
+//  Created by Alisher Saideshov on 17.04.2024.
+//
+
+import Foundation
+
+struct User {
+    let name = "12345"
+    let password = "12345"
+}
--- RXSwiftVIPER/View/ViewController.swift
+++ RXSwiftVIPER/View/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class ViewController: UIViewController {
+
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+
+        self.disposeBag = DisposeBag()
+
+        let scheduler = SerialDispatchQueueScheduler(qos: .default)
+        let subscription = Observable<Int>.interval(.milliseconds(300), scheduler: scheduler)
+            .observe(on: MainScheduler.instance)
+            .subscribe { event in
+                print(event)
+            }
+        Thread.sleep(forTimeInterval: 2)
+
+        subscription.dispose()
     }
 }
 
